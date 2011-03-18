039682071fbdfaf19b61d81ec65c5aa73cab60c8
Jamie Chen <jachen@ucsd.edu>
2011-03-18 10:39:22 -0700

    Add itv test case.

---
+++ ionint/itvtest.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from ion.test.iontest import ItvTestCase
+from twisted.internet import defer
+from ion.services.coi.attributestore import AttributeStoreClient
+
+class AttributeStoreTest(ItvTestCase):
+    services = ["res/apps/attributestore.app"]  # start these apps prior to testing.
+
+    @defer.inlineCallbacks
+    def setUp(self):
+        yield self._start_container()
+
+    @defer.inlineCallbacks
+    def tearDown(self):
+        yield self._stop_container()
+
+    @defer.inlineCallbacks
+    def test_set_attr(self):
+        asc = AttributeStoreClient()
+        yield asc.put("hi", "hellothere")
+
+        res = yield asc.get("hi")
+        self.failUnless(res == "hellothere")
+
+    @defer.inlineCallbacks
+    def test_set_attr2(self):
+    # "hi" is still set here, but only if test_set_attr is run first, be careful
+        asc = AttributeStoreClient()
+        res = yield asc.get("hi")
+        self.failUnless(res == "hellothere")
+
