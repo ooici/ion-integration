f2a33f0433c0242729dda47445dc5ceeaf4d4c1e
David Stuebe <dstuebe@asascience.com>
2011-04-20 10:36:15 -0400

    Added simple test to debug configuration issues.

---
+++ ionint/test/test_itv_trial.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from ion.test.iontest import ItvTestCase
+
+from twisted.internet import defer
+from twisted.trial import unittest
+from ion.test.iontest import IonTestCase
+
+class TestTrial(unittest.TestCase):
+
+    def test_trial(self):
+
+        pass
+
+
+class TestIonTestCase(IonTestCase):
+
+    def test_trial(self):
+
+        pass
+
+
+class TestItvTestCase(ItvTestCase):
+
+    def test_trial(self):
+
+        pass
+
+
