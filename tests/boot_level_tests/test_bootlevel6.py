a7ce66682ca094efb02e11de75f954440856be49
Jamie Chen <jachen@ucsd.edu>
2011-05-14 10:46:06 -0700

    bootlevel6 ping test

---
+++ tests/boot_level_tests/test_bootlevel6.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+@file test_bootlevel6.py
+@test Bootlevel 6 ready checks.
+"""
+
+import ion.util.ionlog
+from twisted.internet import defer
+from ion.test.iontest import ItvTestCase
+from ion.core import ioninit
+from ion.core.process.process import Process
+
+log = ion.util.ionlog.getLogger(__name__)
+CONF = ioninit.config(__name__)
+
+class Bootlevel6ReadyTest(ItvTestCase):
+
+    app_dependencies = ["res/apps/attributestore.app"]
+
+    @defer.inlineCallbacks
+    def setUp(self):
+        yield self._start_container()
+
+    @defer.inlineCallbacks
+    def tearDown(self):
+        yield self._stop_container()
+
+    @defer.inlineCallbacks
+    def test_all_services(self):
+        p = Process()
+        yield p.spawn()
+
+        for servicename in ['attributestore']:
+            (content, headers, msg) = yield p.rpc_send(p.get_scoped_name('system', servicename), 'ping', {})
+            # if timeout, will just fail the test
+
