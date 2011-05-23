80e047bc840a7001bcdcb0ad179c1bbf72c60b59
David Stuebe <dstuebe@asascience.com>
2011-05-22 20:11:54 -0400

    Almost completed new ingestion test...

--- tests/services/dm/test_ingestion.py
+++ tests/services/dm/test_ingestion.py
@@ -13,9 +13,19 @@ from ion.test.iontest import ItvTestCase
 from ion.core import ioninit
 from ion.core.exception import ReceivedApplicationError
 from ion.util import procutils as pu
+from ion.util.iontime import IonTime
+
 from ion.integration.eoi.agent.java_agent_wrapper import JavaAgentWrapperClient
-from ion.services.coi.datastore_bootstrap.ion_preload_config import SAMPLE_PROFILE_DATASET_ID, SAMPLE_PROFILE_DATA_SOURCE_ID
-from ion.services.coi.datastore_bootstrap.ion_preload_config import SAMPLE_CHOPTANK_RIVER_GAUGE_DATASET_ID, SAMPLE_CHOPTANK_RIVER_GAUGE_DATA_SOURCE_ID
+
+from ion.core.process import process
+from ion.services.coi.resource_registry import resource_client
+from ion.services.coi.resource_registry import association_client
+
+from ion.core.object.object_utils import CDM_DATASET_TYPE, CDM_GROUP_TYPE, create_type_identifier
+
+from ion.services.coi.datastore_bootstrap.ion_preload_config import HAS_A_ID
+
+DATA_SOURCE_RESOURCE_TYPE = create_type_identifier(object_id=4503, version=1)
 
 
 log = ion.util.ionlog.getLogger(__name__)
@@ -40,6 +50,13 @@ class IntTestIngest(ItvTestCase):
     @defer.inlineCallbacks
     def setUp(self):
         yield self._start_container()
+        self.proc = process.Process()
+        yield self.proc.spawn()
+
+
+        self.rc = resource_client.ResourceClient(proc=self.proc)
+
+        self.ac = association_client.AssociationClient(proc=self.proc)
 
     @defer.inlineCallbacks
     def tearDown(self):
@@ -47,10 +64,47 @@ class IntTestIngest(ItvTestCase):
 
     @defer.inlineCallbacks
     def test_ingest(self):
+
+
+        dataset = yield self.rc.create_instance(CDM_DATASET_TYPE,
+                                                ResourceName = 'Blank dataset for testing ingestion',
+                                                ResourceDescription= 'An example of a station dataset')
+
+        group = dataset.CreateObject(CDM_GROUP_TYPE)
+        dataset.root_group = group
+
+
+
+        datasource = yield self.rc.create_instance(DATA_SOURCE_RESOURCE_TYPE,
+                                                ResourceName = 'NTAS RT for ingestion testing',
+                                                ResourceDescription= 'An example of a data source for the NTAS RT dataset for testing ingestion')
+
+
+        # Just create it - the workbench/datastore will take care of the rest!
+        asssociation = yield self.ac.create_association(datasource, HAS_A_ID,  dataset)
+
+        datasource.source_type = datasource.SourceType.NETCDF_S
+        datasource.request_type = datasource.RequestType.DAP
+
+        datasource.base_url = "http://geoport.whoi.edu/thredds/dodsC/usgs/data0/rsignell/data/oceansites/OS_NTAS_2010_R_M-1.nc"
+
+        datasource.max_ingest_millis = 6000
+        
+        datasource.registration_datetime_millis = IonTime().time_ms
+
+        datasource.ion_title = "NTAS1 Data Source"
+        datasource.ion_description = "Data NTAS1"
+
+        yield self.rc.put_resource_transaction([dataset, datasource])
+
+        print datasource
+
+        print dataset
+
+
         # get a subscriber going to notification from ingest service
         jawc = JavaAgentWrapperClient()
-        #resp = yield jawc.request_update(SAMPLE_PROFILE_DATASET_ID, SAMPLE_PROFILE_DATA_SOURCE_ID)
-        resp = yield jawc.request_update(SAMPLE_CHOPTANK_RIVER_GAUGE_DATASET_ID, SAMPLE_CHOPTANK_RIVER_GAUGE_DATA_SOURCE_ID)
+        resp = yield jawc.request_update(dataset.ResourceIdentity, datasource.ResourceIdentity)
 
     """
     @defer.inlineCallbacks
