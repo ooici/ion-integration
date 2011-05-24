f6bc79f1f1f05421549b5001f905fae416a68f89
David Stuebe <dstuebe@asascience.com>
2011-05-24 01:26:54 -0400

    Improved ingestion test to check notification result and added rpc and event kickoff tests.

--- tests/services/dm/test_ingestion.py
+++ tests/services/dm/test_ingestion.py
@@ -23,11 +23,17 @@ from ion.services.coi.resource_registry import association_client
 
 from ion.core.object.object_utils import CDM_DATASET_TYPE, CDM_GROUP_TYPE, create_type_identifier
 
+from ion.services.dm.distribution.events import DatasourceUnavailableEventSubscriber, DatasetSupplementAddedEventSubscriber, ScheduleEventPublisher
+
+from ion.services.dm.scheduler.scheduler_service import SCHEDULE_TYPE_PERFORM_INGESTION_UPDATE
+
 from ion.services.coi.datastore_bootstrap.ion_preload_config import HAS_A_ID
 
-DATA_SOURCE_RESOURCE_TYPE = create_type_identifier(object_id=4503, version=1)
-
+DATA_SOURCE_RESOURCE_TYPE   = create_type_identifier(object_id=4503, version=1)
 THREDDS_AUTHENTICATION_TYPE = create_type_identifier(object_id=4504, version=1)
+SCHEDULER_PERFORM_INGEST    = create_type_identifier(object_id=2607, version=1)
+
+
 
 log = ion.util.ionlog.getLogger(__name__)
 CONF = ioninit.config(__name__)
@@ -63,7 +69,20 @@ class IntTestIngest(ItvTestCase):
         yield self._stop_container()
 
     @defer.inlineCallbacks
-    def test_ingest(self):
+    def test_rpc_jaw_ingest(self):
+
+        yield self.jaw_ingest_test(self.rpc_call_jaw)
+
+
+    @defer.inlineCallbacks
+    def test_scheduled_jaw_ingest(self):
+
+        yield self.jaw_ingest_test(self.scheduled_jaw_event)
+
+
+
+    @defer.inlineCallbacks
+    def jaw_ingest_test(self, call_jaw):
 
 
         dataset = yield self.rc.create_instance(CDM_DATASET_TYPE,
@@ -73,7 +92,9 @@ class IntTestIngest(ItvTestCase):
         group = dataset.CreateObject(CDM_GROUP_TYPE)
         dataset.root_group = group
 
+        #group.name = 'David'
 
+        #log.debug(dataset.ResourceObject.PPrint())
 
         datasource = yield self.rc.create_instance(DATA_SOURCE_RESOURCE_TYPE,
                                                 ResourceName = 'CGSN example for ingestion testing',
@@ -104,13 +125,56 @@ class IntTestIngest(ItvTestCase):
 
         yield self.rc.put_resource_transaction([dataset, datasource])
 
+
+        #log.debug(dataset.ResourceObject.Debug())
+
         log.info('Created dataset and datasource for testing')
 
-        #yield pu.asleep(5)
+
+        sub_added = DatasetSupplementAddedEventSubscriber(process=self.proc, origin=dataset.ResourceIdentity)
+        yield sub_added.initialize()
+        yield sub_added.activate()
+
+        sub_added.ondata = lambda msg: test_deferred.callback( msg['content'].additional_data.datasource_id)
+
+        test_deferred = defer.Deferred()
+
+        log.info('Created subscriber to listen for test results')
+
+        yield call_jaw(dataset.ResourceIdentity, datasource.ResourceIdentity)
+
+
+
+        datasource_id = yield test_deferred
+
+        self.assertEqual(datasource_id, datasource.ResourceIdentity)
+
+
+    @defer.inlineCallbacks
+    def rpc_call_jaw(self, dataset_id, datasource_id):
 
         # get a subscriber going to notification from ingest service
         jawc = JavaAgentWrapperClient()
-        resp = yield jawc.request_update(dataset.ResourceIdentity, datasource.ResourceIdentity)
+        resp = yield jawc.request_update(dataset_id, datasource_id)
+
+
+    @defer.inlineCallbacks
+    def scheduled_jaw_event(self, dataset_id, datasource_id):
+
+        pub = ScheduleEventPublisher(process=self.proc)
+
+        msg = yield pub.create_event(origin=SCHEDULE_TYPE_PERFORM_INGESTION_UPDATE,
+                                          task_id="manage_data_resource_FAKED_TASK_ID")
+
+        msg.additional_data.payload = msg.CreateObject(SCHEDULER_PERFORM_INGEST)
+        msg.additional_data.payload.dataset_id     = dataset_id
+        msg.additional_data.payload.datasource_id  = datasource_id
+
+
+        yield pub.publish_event(msg, origin=SCHEDULE_TYPE_PERFORM_INGESTION_UPDATE)
+
+
+
 
     """
     @defer.inlineCallbacks
