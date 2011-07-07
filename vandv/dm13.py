b8b4df1093fc24d53a1b56b17168f9a2ce0221eb
Dave Foster <dfoster@asascience.com>
2011-07-07 19:31:44 -0400

    DM13 tests

---
+++ vandv/dm13.py
@@ -0,0 +1,97 @@
+from twisted.internet import defer
+
+#from ion.util import procutils as pu
+from ion.vandv.vandvbase import VVBase
+import os, os.path, time
+
+from ion.interact.int_observer import InteractionObserver
+from ion.util.os_process import OSProcess
+from ion.services.dm.distribution.events import DatasetSupplementAddedEventSubscriber, IngestionProcessingEventSubscriber
+
+class VVDM13(VVBase):
+    """
+    
+    """
+
+    @defer.inlineCallbacks
+    def setup(self):
+
+        # start full system
+        yield self._start_itv(files=["itv_start_files/boot_level_4_local.itv",
+                                     "itv_start_files/boot_level_5.itv",
+                                     "itv_start_files/boot_level_6.itv",
+                                     "itv_start_files/boot_level_7.itv",
+                                     "itv_start_files/boot_level_8.itv",
+                                     "itv_start_files/boot_level_9.itv",
+                                     "itv_start_files/3x_ingestion.itv",
+                                     "itv_start_files/4x_boot_level_10.itv"])   # four JAWs
+
+        # message observer - spawning here means we don't have to listen to startup stuff
+        self._mo = InteractionObserver()
+        yield self._mo.spawn()
+
+        # supplement added subscriber - we yield on updates here
+        self._def_sup_added = defer.Deferred()
+        self._added_count = 0
+
+        self._sub = DatasetSupplementAddedEventSubscriber(process=self._mo)
+        def ingest_succeeded(msg):
+            self._added_count += 1
+            if self._added_count == 4:
+                self._def_sup_added.callback(self._added_count)
+
+        self._sub.ondata = ingest_succeeded
+
+        yield self._mo.register_life_cycle_object(self._sub)
+
+        # subscriber so we can just see something is happening
+        self._ingest_sub = IngestionProcessingEventSubscriber(process=self._mo)
+        def _print_ingest(dat):
+            print "INGEST (", dat['content'].additional_data.ingestion_process_id, ") PROCESSING:", dat['content'].additional_data.processing_step
+
+        self._ingest_sub.ondata = _print_ingest
+
+        yield self._mo.register_life_cycle_object(self._ingest_sub)
+
+    @defer.inlineCallbacks
+    def _ingest_dataset(self, dsregfile):
+
+        ijr = os.path.join(os.getcwd().rsplit("/", 1)[0], 'ioncore-java-runnables')
+        dsreg = OSProcess(binary=os.path.join(ijr, 'dataset_registration'), startdir=ijr, spawnargs=[os.path.join(os.getcwd(), "vandv", "dm13", dsregfile)])
+        fin = yield dsreg.spawn()
+
+        # pull out dataset id
+        for lines in fin['outlines']:
+            for line in lines.split("\n"):
+                if "data_set_id: " in line:
+                    _, dsid = line.split(" ")
+                    defer.returnValue(dsid.strip().strip("\""))
+                    break
+
+        defer.returnValue(None)
+
+    @defer.inlineCallbacks
+    def s1_ingest_4(self):
+        """
+        1. Start ingestion on four .dsreg files at once
+        """
+
+        #dlist = defer.DeferredList([self._ingest_dataset('ndbc_sos-44014_airtemp.dsreg'),
+        #                            self._ingest_dataset('ndbc_sos-44014_currents.dsreg'),
+        #                            self._ingest_dataset('ndbc_sos-44014_winds.dsreg'),
+        #                            self._ingest_dataset('ndbc_sos-44013_sea_water_electrical_conductivity.dsreg')])
+        yield self._ingest_dataset('ndbc_sos-44014_airtemp.dsreg')
+        yield self._ingest_dataset('ndbc_sos-44014_currents.dsreg')
+        yield self._ingest_dataset('ndbc_sos-44014_winds.dsreg')
+        yield self._ingest_dataset('ndbc_sos-44013_sea_water_electrical_conductivity.dsreg')
+
+        #yield dlist 
+        yield self._def_sup_added
+        self._def_sup_added = defer.Deferred()
+
+        self._added_count = 0
+
+        print "datasets"#, dlist
+
+
+
