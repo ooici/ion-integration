df358f2d82bf6d720f680dfa1ae9da578465abae
David Stuebe <dstuebe@asascience.com>
2011-07-08 11:06:52 -0400

    Just about there on DM 12. Need to combine with DM 11 &  22

--- vandv/dm12.py
+++ vandv/dm12.py
@@ -4,6 +4,7 @@ from twisted.internet import defer
 from ion.vandv.vandvbase import VVBase
 import os, os.path, time
 
+from ion.core.object.object_utils import sha1_to_hex
 from ion.interact.int_observer import InteractionObserver
 from ion.util.os_process import OSProcess
 from ion.core.process.process import Process
@@ -11,6 +12,15 @@ from ion.services.coi.resource_registry.resource_client import ResourceClient
 from ion.services.dm.distribution.events import DatasetSupplementAddedEventSubscriber, IngestionProcessingEventSubscriber
 
 from ion.services.coi.datastore_bootstrap.ion_preload_config import SAMPLE_PROFILE_DATASET_ID
+
+from ion.core.data.cassandra_bootstrap import CassandraStoreBootstrap, CassandraIndexedStoreBootstrap
+from ion.core.data.storage_configuration_utility import get_cassandra_configuration, STORAGE_PROVIDER, PERSISTENT_ARCHIVE,BLOB_CACHE, COMMIT_CACHE
+
+import binascii
+
+from ion.core import ioninit
+CONF = ioninit.config(__name__)
+
 
 class VVDM12(VVBase):
     """
@@ -23,11 +33,31 @@ class VVDM12(VVBase):
     def setup(self):
 
         # start full system
-        yield self._start_itv(files=["itv_start_files/boot_level_4_local.itv",
+        yield self._start_itv(files=["itv_start_files/boot_level_4.itv",
                                      ])
         self.proc = Process()
         yield self.proc.spawn()
         self.rc = ResourceClient(proc=self.proc)
+
+
+        uname = CONF.getValue('cassandra_username', None)
+        pword = CONF.getValue('cassandra_password', None)
+
+        self._storage_conf = get_cassandra_configuration()
+
+        storage_provider = self._storage_conf[STORAGE_PROVIDER]
+        keyspace = self._storage_conf[PERSISTENT_ARCHIVE]['name']
+
+        self.commit_store = CassandraIndexedStoreBootstrap(uname, pword, storage_provider, keyspace, COMMIT_CACHE)
+        self.blob_store = CassandraStoreBootstrap(uname, pword, storage_provider, keyspace, BLOB_CACHE)
+
+        yield self.commit_store.initialize()
+        yield self.commit_store.activate()
+
+        yield self.blob_store.initialize()
+        yield self.blob_store.activate()
+
+
 
     @defer.inlineCallbacks
     def s1_get_dataset(self):
@@ -36,7 +66,17 @@ class VVDM12(VVBase):
         """
 
         self.dset = yield self.rc.get_instance(SAMPLE_PROFILE_DATASET_ID)
-        print self.dset.root_group.attributes.PPrint(name='Attribute')
+
+        repo = self.dset.Repository
+
+        print '==== Current Version of Dataset: ===='
+        print '= Dataset ID: %s' % repo.repository_key
+        print '= Dataset Branch: %s' % repo.current_branch_key()
+        print '= Current Commit: %s' % sha1_to_hex(repo.commit_head.MyId)
+        print '= Current number of attributes in dataset: %d' % len(self.dset.root_group.attributes)
+        print '====================================='
+
+        #print self.dset.root_group.attributes.PPrint(name='Attribute')
 
 
     def s2_add_dataset_attribute(self):
@@ -44,20 +84,88 @@ class VVDM12(VVBase):
         2. Add a new global attribute to the dataset and print the attributes again
         """
         group = self.dset.root_group
-        group.AddAttribute('NewAttribute', group.DataType.STRING, 'ASA OOICI team kicks ass!')
+        attr = group.AddAttribute('NewAttribute', group.DataType.STRING, 'ASA OOICI team kicks ass!')
 
-        print self.dset.root_group.attributes.PPrint(name='Attribute')
+        print 'Added new attribute:'
+        print attr.PPrint()
 
+        self.attr = attr
+
+        repo = self.dset.Repository
+        repo.commit('Commiting dataset changes for VV DM12 step 2')
+
+        print '==== New Attribute ID and Content: ===='
+        print '= Attribute CASRef: %s' % sha1_to_hex(self.attr.MyId)
+        se = repo.index_hash.get(self.attr.MyId)
+        print 'Attribute blob: %s' % binascii.b2a_base64(se.value)
+
+        print '= Attribute CASRef: %s' % sha1_to_hex(self.attr.array.MyId)
+        se = repo.index_hash.get(self.attr.array.MyId)
+        print 'Attribute array blob: %s' % binascii.b2a_base64(se.value)
+        print '======================================='
 
 
     @defer.inlineCallbacks
-    def s2_persist_dataset_changes(self):
+    def s3_persist_dataset_changes(self):
         """
-        2. Push the dataset to the datastore for persistence and print the keys of the new version and the new attributes
+        3. Push the dataset to the datastore for persistence and print the keys of the new version and the new attributes
         """
 
         yield self.rc.put_instance(self.dset)
         repo = self.dset.Repository
 
-        print 'Dataset Branch: %s' % repo.current_branch_key
-        print 'Dataset ID: %s' % repo.repository_key
+        print '==== New Version of Dataset: ===='
+        print '= Dataset ID: %s' % repo.repository_key
+        print '= Dataset Branch: %s' % repo.current_branch_key()
+        print '= Current Commit: %s' % sha1_to_hex(repo.commit_head.MyId)
+        print '= Parent Commit: %s' % sha1_to_hex(repo.commit_head.parentrefs[0].commitref.MyId)
+        print '================================='
+
+
+
+    @defer.inlineCallbacks
+    def s4_extract_and_examine_persisted_elements(self):
+        """
+        4. Get the blobs from cassandra using a separate connection to show they were persisted
+        """
+        repo = self.dset.Repository
+
+
+        # Get the blobs stored in cassandra directly and print to screen
+        print '==== Cassandra Content: ===='
+        blob = yield self.blob_store.get(self.attr.MyId)
+        print 'Attribute blob: %s' % binascii.b2a_base64(blob)
+
+        blob = yield self.blob_store.get(self.attr.MyId)
+        print 'Attribute array blob: %s' % binascii.b2a_base64(blob)
+
+        blob = yield self.commit_store.get(repo.commit_head.MyId)
+        print 'Commit blob: %s' % binascii.b2a_base64(blob)
+
+        print '============================'
+
+
+    def s5_show_previous_versions(self):
+        """
+        5. Show the commit log for the dataset
+        """
+        repo = self.dset.Repository
+
+        print 'Commit History:'
+        print repo.list_parent_commits()
+
+
+    @defer.inlineCallbacks
+    def teardown(self):
+
+        print 'Tearing Down Cassandra Column Families'
+        # Don't let the test script persist anything between tests.
+        yield self.blob_store.client.truncate(self.blob_store._cache_name)
+        yield self.commit_store.client.truncate(self.commit_store._cache_name)
+
+        self.blob_store.terminate()
+        self.commit_store.terminate()
+
+        yield VVBase.teardown(self)
+
+        
